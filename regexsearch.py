1cd9e6737ee3995c228a0b3afdddb7ac07361e8f
usersts <usersts>
2017-04-06 16:47:54 -0300

    rextxtsearch

---
+++ regexsearch.py
@@ -0,0 +1,12 @@
+import os, re
+filteri= input("Enter the Filter: ")
+arqpath = input("Enter the Path: ")
+filterre = re.compile(r'%s'%filteri)
+result = []
+for filename in os.listdir(arqpath):
+    if filename.endswith(".txt"):
+        my_textfile = open(os.path.basename(arqpath+filename), 'r')
+        for line in my_textfile:
+            result = filterre.search(line)
+        my_textfile.close()
+print (result)
