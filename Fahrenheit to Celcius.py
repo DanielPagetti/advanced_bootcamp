348ad98e125c3e9a40bf35597282c3763d80876c
usersts <usersts>
2017-04-06 10:41:46 -0300

    update1

--- Fahrenheit to Celcius.py
+++ Fahrenheit to Celcius.py
@@ -5,10 +5,10 @@ def convert(temp):
     temp_converted = 0.00
     if temp.lower() == "f":
         temp_fahrenheit = input("Enter Temperature in Fahrenheit:")
-        temp_converted= (float(temp_fahrenheit)-32)*5/9
+        temp_converted= "Celcius:"+str((float(temp_fahrenheit)-32)*5/9)
     elif temp.lower() == "c":
         temp_celcius = input("Enter Temperature in Celcius:")
-        temp_converted= (float(temp_celcius)*9/5)+32
+        temp_converted= "Fahrenheit:" + str((float(temp_celcius)*9/5)+32)
     return temp_converted
 
 print (convert(is_temp))
