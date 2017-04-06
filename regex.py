ccb201d6365ab48a3df591a13960fb642767a879
usersts <usersts>
2017-04-06 13:47:55 -0300

    phone and mail test

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
 
-phoneNumRegex = re.compile(r'\S\S\S\S\S@\S\S.\S\S\S.\S\S\S')
+phoneNumRegex = re.compile(r'\S+@\S+.\S\S\S.\S\S\S')
 mo = phoneNumRegex.search('My number dofJLajlkajdkjAF; dpagetti@br.ibm.com is 415-555-4242 SAKDJLajflkAJF;Lkjlksadjsalkdj.')
 
 print (mo.group())
