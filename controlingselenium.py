d8feb04b0c0597bc322a9816ad54a3cfa708c064
usersts <usersts>
2017-04-07 16:35:42 -0300

    excell2

--- controlingselenium.py
+++ controlingselenium.py
@@ -13,3 +13,22 @@ try:
     print('Found <%s> element with that class name!' % (elem.tag_name))
 except:
     print('Was not able to find an element with that name.')
+
+linkElem = browser.find_element_by_link_text('Read It Online')
+type(linkElem)
+linkElem.click()
+
+browser = webdriver.Firefox()
+browser.get('https://mail.yahoo.com')
+emailElem = browser.find_element_by_id('login-username')
+emailElem.send_keys('not_my_real_email')
+passwordElem = browser.find_element_by_id('login-passwd')
+passwordElem.send_keys('12345')
+passwordElem.submit()
+
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Firefox()
+browser.get('http://nostarch.com')
+htmlElem = browser.find_element_by_tag_name('html')
+htmlElem.send_keys(Keys.END)     # scrolls to bottom
+htmlElem.send_keys(Keys.HOME)    # scrolls to top
