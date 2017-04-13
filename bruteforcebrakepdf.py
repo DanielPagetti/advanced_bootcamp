90c65b229d30d26c12ff03f0e5e306ed908a58eb
usersts <usersts>
2017-04-13 10:52:04 -0300

    brute

---
+++ bruteforcebrakepdf.py
@@ -0,0 +1,17 @@
+import PyPDF2, os
+
+pdfReader = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
+pdfReader.isEncrypted
+True
+
+my_textfile = open(os.path.basename('dictionary.txt'), 'r')
+decr = False
+for line in my_textfile:
+    decr = pdfReader.decrypt(line)
+    if decr == True:
+        print (line)
+        pdfReader.getPage(0)
+my_textfile.close()
+
+
+
