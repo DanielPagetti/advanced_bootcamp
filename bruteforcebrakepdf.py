bf5986ffbbc5f70e5bbd97ef18da50796c21d1ef
usersts <usersts>
2017-04-20 14:20:56 -0300

    update

--- bruteforcebrakepdf.py
+++ bruteforcebrakepdf.py
@@ -8,6 +8,7 @@ my_textfile = open(os.path.basename('dictionary.txt'), 'r')
 decr = False
 for line in my_textfile:
     decr = pdfReader.decrypt(line)
+    print (line)
     if decr == True:
         print (line)
         pdfReader.getPage(0)
